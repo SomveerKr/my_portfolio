112f6a5939b9c1230e302aac5164794dd38813f1
Somveer Kumar <main.somveerkumar@gmail.com>
2026-02-20 22:59:46 +0530

    fix sanity/image-url has been deprecated warning

--- frontend_nextjs/lib/sanity.js
+++ frontend_nextjs/lib/sanity.js
@@ -1,5 +1,5 @@
 import { createClient } from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import { createImageUrlBuilder } from '@sanity/image-url';
 
 export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
@@ -8,5 +8,5 @@ export const client = createClient({
     useCdn: true,
 });
 
-const builder = imageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
